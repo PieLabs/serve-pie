8dd26507219ebcc787348f8bd936008a776694b7
Ed Eustace <ed.eustace@gmail.com>
2016-05-27 16:30:30 +0100

    flesh out the specs

--- test/spec/bower/bower-dir-spec.js
+++ test/spec/bower/bower-dir-spec.js
@@ -2,6 +2,27 @@ const should = require('should');
 const sinon = require('sinon');
 const proxyquire = require('proxyquire');
 
+require('../../../lib/log-factory').setDefaultLevel('info');
+
+function MockEmitter(emitterName) {
+  var handlers = {};
+
+  this.on = function (name, handler) {
+    handlers[name] = handler;
+    return this;
+  };
+
+  this.trigger = function () {
+    let args = Array.prototype.slice.call(arguments);
+    let name = args.shift();
+    if (handlers[name]) {
+      handlers[name].apply(null, args);
+    } else {
+      throw new Error('no handler for: ', name);
+    }
+  };
+}
+
 describe('bower-dir', () => {
 
   "use strict";
@@ -9,55 +30,50 @@ describe('bower-dir', () => {
   let BowerDir, bowerDir;
   let childProcess, fsExtra, bower, installEmitter, resolutionReport;
   beforeEach(() => {
-    
-    installEmitter = {};
-    installEmitter.on = (name, handler) => {
-      
-      if(name === 'end'){
-        handler({a: {}, b: {}});
-      }
-      return installEmitter;
-    }; 
-    
+
+    installEmitter = new MockEmitter('install');
+
     bower = {
       commands: {
         install: sinon.stub().returns(installEmitter)
-      } 
+      }
     };
-    
+
     childProcess = {
       exec: sinon.stub().callsArgWith(1, null)
     };
+
     fsExtra = {
       existsSync: sinon.stub(),
       readJSONSync: sinon.stub().returns({}),
       writeJSONSync: sinon.stub(),
       unlinkSync: sinon.stub(),
       openSync: sinon.stub()
-    }
-    
+    };
+
     resolutionReport = {
       fromPath: sinon.stub().returns({})
-    }
-    
+    };
+
     BowerDir = proxyquire('../../../lib/bower/bower-dir', {
       'child_process': childProcess,
       'fs-extra': fsExtra,
-      'bower' : bower,
-      'bower-config' : {
-        read: function(){
-          return {directory: 'bower_components'}
+      'bower': bower,
+      'bower-config': {
+        read: function () {
+          return { directory: 'bower_components' }
         }
       },
-      './resolution-report' : resolutionReport
+      './resolution-report': resolutionReport
     });
     bowerDir = new BowerDir('dir');
   });
 
+   
   describe('components', () => {
     it('returns the components', () => {
       bowerDir.components().should.eql('dir/bower_components')
-    })
+    });
   });
 
   describe('clean', () => {
@@ -95,37 +111,40 @@ describe('bower-dir', () => {
       fsExtra.existsSync.onCall(0).returns(true).onCall(1).returns(true);
       bowerDir.isInstalled().should.eql(true);
     });
-    
+
     it('returns false when comps exist and report does not', () => {
       fsExtra.existsSync.onCall(0).returns(true).onCall(1).returns(false);
       bowerDir.isInstalled().should.eql(false);
     });
-    
+
     it('returns false when comps dont exist and the report does', () => {
       fsExtra.existsSync.onCall(0).returns(false).onCall(1).returns(true);
       bowerDir.isInstalled().should.eql(false);
     });
   });
-  
+
   describe('localInstall', () => {
-    
-    let result;     
+
+    let result;
     beforeEach((done) => {
-      bowerDir.localInstall({a: 'path/to/a'})
+
+      bowerDir.localInstall({ a: 'path/to/a' })
         .then((obj) => {
           result = obj;
           done();
         })
         .catch(done);
+
+      installEmitter.trigger('end', { a: {}, b: {} });
     });
     it('calls bower.commands.install', () => {
       sinon.assert.calledWith(bower.commands.install, [], sinon.match.any, {});
     });
-    
+
     it('calls resolutionReport.fromPath', () => {
       sinon.assert.called(resolutionReport.fromPath);
     });
-    
+
     it('writes out the tmp bower.json', () => {
       sinon.assert.calledWith(fsExtra.writeJSONSync, 'dir/bower.json', {
         name: 'tmp-bower-config',
@@ -135,17 +154,117 @@ describe('bower-dir', () => {
         }
       });
     });
-    
+
     it('removes the tmp bower.json', () => {
       sinon.assert.calledWith(fsExtra.unlinkSync, 'dir/bower.json');
     });
-    
+
     it('writes out the report', () => {
-      sinon.assert.calledWith(fsExtra.writeJSONSync, 'dir/.bower.installed.json', {a: {}, b: {}});
+      sinon.assert.calledWith(fsExtra.writeJSONSync, 'dir/.bower.installed.json', { a: {}, b: {} });
     });
   });
   
-  describe('link', () => {
+  
+  describe('linkDependencies', () => {
     
+    beforeEach((done) => {
+      
+      fsExtra.lstatSync = sinon.stub().returns({isDirectory: () => true});
+      fsExtra.existsSync = sinon.stub().returns(true); 
+      bowerDir.link = sinon.stub().returns(Promise.resolve({}));
+      bowerDir.linkDependencies({
+        name: '../../'
+      })
+      .then(() =>{
+        done();
+      })
+      .catch(() => {
+        done();
+      });
+    });   
+    
+    it('iterates through then links', () => {
+      
+    });
+    
+  });
+  
+  describe('link', () => {
+
+    let successful;
+    let link, linkPromise, firstEmitter, secondEmitter;
+
+    beforeEach(() => {
+      firstEmitter = new MockEmitter('first');
+      secondEmitter = new MockEmitter('second');
+      let stub = sinon.stub();
+      stub.onCall(0).returns(firstEmitter);
+      stub.onCall(1).returns(secondEmitter);
+      bower.commands.link = stub;
+      linkPromise = bowerDir.link('name', '../');
+    });
+
+    describe('when dependency link fails', () => {
+
+      let error;
+
+      beforeEach((done) => {
+        linkPromise
+          .catch((e) => {
+            error = e;
+            done();
+          });
+        firstEmitter.trigger('error', new Error('e'));
+      });
+
+      it('returns the error in the catch', () => {
+        error.should.eql(new Error('e'));
+      });
+    });
+    
+    describe('when current link to dependency fails', () => {
+
+      let error;
+
+      beforeEach((done) => {
+        linkPromise
+          .catch((e) => {
+            error = e;
+            done();
+          });
+        firstEmitter.trigger('end');
+        secondEmitter.trigger('error', new Error('e'));
+      });
+
+      it('returns the error in the catch', () => {
+        error.should.eql(new Error('e'));
+      });
+    });
+
+    describe('when successful', () => {
+
+      beforeEach((done) => {
+        linkPromise
+          .then((s) => {
+            successful = s;
+            done();
+          });
+        firstEmitter.trigger('end');
+        secondEmitter.trigger('end');
+      });
+
+      it('links successfully', () => {
+        successful.should.eql(true);
+      });
+
+      it('calls link on the dependency', () => {
+        bower.commands.link.calledWith();
+      });
+
+      it('calls link for the dependency on the current dir', () => {
+        bower.commands.link.calledWith('name');
+      });
+
+    });
   });
 });
