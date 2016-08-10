d34d69fe5ef9c034b57e72ffe387776a798851e2
edeustace <ed.eustace@gmail.com>
2016-08-10 17:31:30 +0100

    fix/update tests

--- test/spec/services/processor/read-pie-processors-spec.js
+++ test/spec/services/processor/read-pie-processors-spec.js
@@ -3,53 +3,65 @@ const should = require('should');
 const sinon = require('sinon');
 
 describe('read-pie-processors', () => {
-  
-  let read;
-  
-  let mockScript; 
-     
+
+  let read, mockScript, fsExtra;
+
   beforeEach((done) => {
-    
+
     mockScript = {
-      runInNewContext: function(sandbox){
-        sandbox.exports.createOutcome = function(){
-          return {correctness: 'correct'};
+      runInNewContext: function (sandbox) {
+        sandbox.exports.createOutcome = function () {
+          return { correctness: 'correct' };
         };
       }
     };
-    
+
+    fsExtra = {
+      readJsonSync: sinon.stub().returns({
+        name: 'name',
+        version: 'version'
+      }),
+      readFileSync: sinon.stub().returns(''),
+      existsSync: sinon.stub().returns(true)
+    };
+
     read = proxyquire('../../../../lib/services/processor/read-pie-processors', {
-      'fs-extra' : {
-        readJsonSync: sinon.stub().returns({
-          name: 'name',
-          version: 'version'
-        }),
-        readFileSync: sinon.stub().returns('')
-      },
-      'vm' : {
-        Script: function() { return mockScript; }
+      'fs-extra': fsExtra,
+      'vm': {
+        Script: function () { return mockScript; }
       }
-    });    
-    
-    read({'comp': ''}, 'bower-dir')
+    });
+
+    read({ 'comp': '' }, 'bower-dir')
       .then((result) => {
-        processors = result; 
+        processors = result;
         done();
       })
       .catch(done);
-          
+
   });
-   
+
   it('should return the processors', () => {
     processors.length.should.eql(1);
   });
-  
+
   it('should return the name and version in the id', () => {
-    processors[0].component.should.eql({name: 'name', version: 'version'}); 
+    processors[0].component.should.eql({ name: 'name', version: 'version' });
   });
-  
-  it('should return the name and version in the id', () => {
-    processors[0].processor.createOutcome().should.eql({correctness: 'correct'}); 
+
+  it('loads controller.js', () => {
+    sinon.assert.calledWith(fsExtra.readFileSync, 'bower-dir/comp/controller.js');
   });
-  
+
+  it('loads legacy processing.js if controller.js doesnt exist', (done) => {
+    fsExtra.existsSync.reset();
+    fsExtra.existsSync.onCall(0).returns(false);
+    fsExtra.existsSync.onCall(1).returns(true);
+    read({ 'comp': '' }, 'bower-dir')
+      .then((result) => {
+        sinon.assert.calledWith(fsExtra.readFileSync, 'bower-dir/comp/processing.js');
+        done();
+      })
+      .catch(done);
+  });
 });
